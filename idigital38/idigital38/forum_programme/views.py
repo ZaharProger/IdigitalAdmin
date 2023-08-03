6512b56f7bcc3b7aef29077d7cf63ee084378888
ZaharDomolego <domolegozahar@gmail.com>
2023-08-04 02:04:51 +0800

    Добавил PUT хэндлер для ProgrammeDay

--- idigital38/idigital38/forum_programme/views.py
+++ idigital38/idigital38/forum_programme/views.py
@@ -6,9 +6,9 @@ from rest_framework.permissions import IsAuthenticated
 
 from django.db.transaction import atomic
 
-from .models import ProgrammeDay
+from .models import ProgrammeDay, DayTimetable, DayBlock, Report
 from .forms import DayBlockForm, ProgrammeDayForm, ReportForm, DayTimetableForm
-from .serializers import ProgrammeDaySerializer
+from .serializers import ProgrammeDaySerializer, DayTimetableSerializer, DayBlockSerializer, ReportSerializer
 
 # Это я тесты проводил для POST хэндлера, можно закомментить, в гит я не закидывал тестовый файл
 from .test import request_data
@@ -60,16 +60,16 @@ class ProgrammeDayView(APIView):
         с последующим сохранением в БД
     """
 
-    def save_nested_data(self, main_form, nested_form_key, fk_key, fk_value, callback) -> dict:
-        inserted_data = {}
+    def add_nested_data(self, data, fk_key, fk_value, callback) -> list:
+        inserted_data = []
 
-        nested_form_data = main_form[nested_form_key] if nested_form_key in main_form.keys() else []
-        for item in nested_form_data:
+        for item in data:
             item[fk_key] = fk_value
             item_form = callback(item)
             if item_form.is_valid():
                 saved_item = item_form.save()
-                inserted_data[saved_item.id] = item
+                item['id'] = saved_item.id
+                inserted_data.append(item)
 
         return inserted_data
 
@@ -78,6 +78,8 @@ class ProgrammeDayView(APIView):
             'name': request_data['name'] if 'name' in request_data.keys() else None,
             'place': request_data['place'] if 'place' in request_data.keys() else None
         }
+        timetable_form_data = request_data['day_timetable'] if 'day_timetable' in request_data.keys() else []
+        blocks_form_data = request_data['day_blocks'] if 'day_blocks' in request_data.keys() else []
 
         day_form = ProgrammeDayForm(day_form_data)
         all_correct = False
@@ -85,26 +87,23 @@ class ProgrammeDayView(APIView):
             with atomic():
                 last_inserted_id = day_form.save()
 
-                self.save_nested_data(
-                    main_form=request_data,
-                    nested_form_key='day_timetable',
+                self.add_nested_data(
+                    data=timetable_form_data,
                     fk_key='day',
                     fk_value=last_inserted_id,
                     callback=lambda form: DayTimetableForm(form)
                 )
-                inserted_blocks = self.save_nested_data(
-                    main_form=request_data,
-                    nested_form_key='day_blocks',
+                inserted_blocks = self.add_nested_data(
+                    data=blocks_form_data,
                     fk_key='day',
                     fk_value=last_inserted_id,
                     callback=lambda form: DayBlockForm(form)
                 )
-                for inserted_block_id, inserted_block_data in inserted_blocks.items():
-                    self.save_nested_data(
-                        main_form=inserted_block_data,
-                        nested_form_key='reports',
+                for inserted_block in inserted_blocks:
+                    self.add_nested_data(
+                        data=inserted_block['reports'] if 'reports' in inserted_block else [],
                         fk_key='block',
-                        fk_value=inserted_block_id,
+                        fk_value=inserted_block['id'],
                         callback=lambda form: ReportForm(form)
                     )
                 all_correct = True
@@ -112,5 +111,98 @@ class ProgrammeDayView(APIView):
         return Response(
             {'message': ''},
             status=status.HTTP_200_OK if all_correct else status.HTTP_400_BAD_REQUEST,
+            content_type='application/json'
+        )
+
+    def update_nested_data(self, data, callback) -> list:
+        updated_data = []
+
+        for item in data:
+            serialized_item = callback(item['id'], item)
+            if serialized_item.is_valid():
+                saved_item = serialized_item.save()
+                item['id'] = saved_item.id
+                updated_data.append(item)
+
+        return updated_data
+
+    def put(self, request):
+        day_form_data = {
+            'id': request_data['id'] if 'id' in request_data.keys() else None,
+            'name': request_data['name'] if 'name' in request_data.keys() else None,
+            'place': request_data['place'] if 'place' in request_data.keys() else None
+        }
+        timetable_form_data = request_data['day_timetable'] if 'day_timetable' in request_data.keys() else []
+        blocks_form_data = request_data['day_blocks'] if 'day_blocks' in request_data.keys() else []
+
+        try:
+            day_id = int(day_form_data['id'])
+            found_day = ProgrammeDay.objects.get(pk=day_id)
+            serialized_day = ProgrammeDaySerializer(found_day, data=day_form_data, partial=True)
+
+            with atomic():
+                if serialized_day.is_valid():
+                    serialized_day.save()
+
+                self.update_nested_data(
+                    data=[item for item in timetable_form_data if 'id' in item.keys()],
+                    callback=lambda item_id, item_data: DayTimetableSerializer(
+                        DayTimetable.objects.get(pk=int(item_id)),
+                        data=item_data,
+                        partial=True
+                    )
+                )
+                updated_blocks = self.update_nested_data(
+                    data=[item for item in blocks_form_data if 'id' in item.keys()],
+                    callback=lambda item_id, item_data: DayBlockSerializer(
+                        DayBlock.objects.get(pk=int(item_id)),
+                        data={item_key: item_data[item_key] for item_key in item_data.keys() if item_key != 'reports'},
+                        partial=True
+                    )
+                )
+                for updated_block_data in updated_blocks:
+                    self.update_nested_data(
+                        data=[item for item in (updated_block_data['reports']
+                                                if 'reports' in updated_block_data else []) if 'id' in item.keys()],
+                        callback=lambda item_id, item_data: ReportSerializer(
+                            Report.objects.get(pk=int(item_id)),
+                            data=item_data,
+                            partial=True
+                        )
+                    )
+
+                self.add_nested_data(
+                    data=[item for item in timetable_form_data if 'id' not in item.keys()],
+                    fk_key='day',
+                    fk_value=found_day.id,
+                    callback=lambda form: DayTimetableForm(form)
+                )
+                inserted_blocks = self.add_nested_data(
+                    data=[item for item in blocks_form_data if 'id' not in item.keys()],
+                    fk_key='day',
+                    fk_value=found_day.id,
+                    callback=lambda form: DayBlockForm(form)
+                )
+                filtered_blocks = [item for item in blocks_form_data if 'id' in item.keys()]
+                filtered_blocks.extend(inserted_blocks)
+                for block in filtered_blocks:
+                    self.add_nested_data(
+                        data=[item for item in (block['reports']
+                                                if 'reports' in block else []) if 'id' not in item.keys()],
+                        fk_key='block',
+                        fk_value=block['id'],
+                        callback=lambda form: ReportForm(form)
+                    )
+
+            message = ''
+            response_status = status.HTTP_200_OK
+
+        except (ProgrammeDay.DoesNotExist, ValueError, TypeError):
+            message = 'Не найден день программы форума по заданному идентификатору'
+            response_status = status.HTTP_404_NOT_FOUND
+
+        return Response(
+            {'message': message},
+            status=response_status,
             content_type='application/json'
         )
