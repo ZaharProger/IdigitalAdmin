5e683390944dcecbf3675a895814a50d33dc3bc4
ZaharDomolego <domolegozahar@gmail.com>
2023-08-03 20:05:07 +0800

    Добавил POST хэндлер в ProgrammeDay

--- idigital38/idigital38/forum_programme/views.py
+++ idigital38/idigital38/forum_programme/views.py
@@ -4,9 +4,14 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from .models import DayTimetable, DayBlock, ProgrammeDay, Report
+from django.db.transaction import atomic
+
+from .models import ProgrammeDay
 from .forms import DayBlockForm, ProgrammeDayForm, ReportForm, DayTimetableForm
-from .serializers import DayBlockSerializer, ReportSerializer, ProgrammeDaySerializer, DayTimetableSerializer
+from .serializers import ProgrammeDaySerializer
+
+# Это я тесты проводил для POST хэндлера, можно закомментить, в гит я не закидывал тестовый файл
+from .test import request_data
 
 
 class ProgrammeDayView(APIView):
@@ -46,5 +51,66 @@ class ProgrammeDayView(APIView):
         return Response(
             {'message': 'Идентификаторы должны быть целыми числами' if not is_valid else ''},
             status=status.HTTP_200_OK if is_valid else status.HTTP_400_BAD_REQUEST,
+            content_type='application/json'
+        )
+
+    """
+        Здесь происходит извлечение вложенных данных из словаря, добавление внешнего ключа для связки в БД в
+        каждый вложенный экземпляр и упаковка в джанго форму (описывается в параметре callback) 
+        с последующим сохранением в БД
+    """
+
+    def save_nested_data(self, main_form, nested_form_key, fk_key, fk_value, callback) -> dict:
+        inserted_data = {}
+
+        nested_form_data = main_form[nested_form_key] if nested_form_key in main_form.keys() else []
+        for item in nested_form_data:
+            item[fk_key] = fk_value
+            item_form = callback(item)
+            if item_form.is_valid():
+                saved_item = item_form.save()
+                inserted_data[saved_item.id] = item
+
+        return inserted_data
+
+    def post(self, request):
+        day_form_data = {
+            'name': request_data['name'] if 'name' in request_data.keys() else None,
+            'place': request_data['place'] if 'place' in request_data.keys() else None
+        }
+
+        day_form = ProgrammeDayForm(day_form_data)
+        all_correct = False
+        if day_form.is_valid():
+            with atomic():
+                last_inserted_id = day_form.save()
+
+                self.save_nested_data(
+                    main_form=request_data,
+                    nested_form_key='day_timetable',
+                    fk_key='day',
+                    fk_value=last_inserted_id,
+                    callback=lambda form: DayTimetableForm(form)
+                )
+                inserted_blocks = self.save_nested_data(
+                    main_form=request_data,
+                    nested_form_key='day_blocks',
+                    fk_key='day',
+                    fk_value=last_inserted_id,
+                    callback=lambda form: DayBlockForm(form)
+                )
+                for inserted_block_id, inserted_block_data in inserted_blocks.items():
+                    self.save_nested_data(
+                        main_form=inserted_block_data,
+                        nested_form_key='reports',
+                        fk_key='block',
+                        fk_value=inserted_block_id,
+                        callback=lambda form: ReportForm(form)
+                    )
+                all_correct = True
+
+        return Response(
+            {'message': ''},
+            status=status.HTTP_200_OK if all_correct else status.HTTP_400_BAD_REQUEST,
             content_type='application/json'
         )
